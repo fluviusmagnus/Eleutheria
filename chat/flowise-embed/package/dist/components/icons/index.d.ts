14bbd48b53d337189b3498ee092d0995d83654b4
人間夜行 <ich@eleutheria.tech>
2024-08-06 00:44:37 +0200

    Switch to Dify service provider.

--- chat/flowise-embed/package/dist/components/icons/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from './AddImageIcon';
-export * from './CircleDotIcon';
-export * from './DeleteIcon';
-export * from './RecordIcon';
-export * from './SendIcon';
-export * from './TrashIcon';
-export * from './XIcon';
-//# sourceMappingURL=index.d.ts.map
